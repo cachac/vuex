cb19e9bf7516600ec47c20179483409e1da745b1
cachac <carlos.echc11@gmail.com>
2019-08-18 16:26:04 -0600

    03

--- src/store.js
+++ src/store.js
@@ -1,10 +1,18 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Car Ch' },
+    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community']
+  },
   mutations: {},
-  actions: {}
-})
+  actions: {},
+  getters:{
+    catLength: state => {
+      return state.categories.length
+    }
+  }
+});
